70439e62ef4c09c4b5c308b943c7695a0f33cb03
sathindu123456@gmail.com <sathindu123456@gmail.com>
2021-06-03 17:39:56 +0530

    final

---
+++ mill.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: db:3306
+-- Generation Time: May 28, 2021 at 01:09 PM
+-- Server version: 8.0.25
+-- PHP Version: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mill`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `price`
+--
+
+CREATE TABLE `price` (
+  `code` int NOT NULL,
+  `price` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `price`
+--
+
+INSERT INTO `price` (`code`, `price`) VALUES
+(0, 1),
+(1, 560),
+(2, 750),
+(3, 795),
+(4, 70);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `code` int NOT NULL,
+  `name` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`code`, `name`) VALUES
+(0, 'Cash'),
+(1, 'Oil Bottle'),
+(2, 'Oil Litter'),
+(3, 'Oil Killo'),
+(4, 'Punak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `code` int NOT NULL,
+  `unit` int NOT NULL,
+  `quantity` decimal(10,0) NOT NULL,
+  `total` double NOT NULL,
+  `date` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `price`
+--
+ALTER TABLE `price`
+  ADD UNIQUE KEY `code` (`code`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD UNIQUE KEY `code` (`code`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
